038bd32159d6a96d9fd0aca7f30cad21df84013d
Milena Shishkova <shishkova.milena@gmail.com>
2017-10-06 01:51:57 +0300

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
+const browserify = require('gulp-browserify');
+
+gulp.task('compile:js', () => {
+    return gulp.src(['./public/scripts/*.js',
+            '!./public/scripts/handlebars-v4.0.10.js',
+            '!./public/scripts/jquery-3.2.1.min.js',
+            './public/scripts/sammy-latest.min.js'
+        ])
+        .pipe(babel({ presets: ['env'] }))
+        .pipe(gulp.dest('./tеmp/scripts'));
+});
+
+gulp.task('minify:css', () => {
+    return gulp.src(['./public/styles/*.css'])
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('minify:js', ['compile:js'], () => {
+    return pump([
+        gulp.src('./tmp/scripts/*.js'),
+        uglify(),
+        gulp.dest('./dist/scripts')
+    ]);
+});
+
+gulp.task('browserify:js', ['minify:js'], () => {
+    return gulp.src('./dist/scripts/main.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('./dist/scripts'));
+});
+
+
+gulp.task('build', ['minify:css', 'browserify:js'], () => {});
